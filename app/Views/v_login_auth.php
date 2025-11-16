d67722ec510a6fa42e8e3bb2c15e5d1516040f6e
Akazano_abe <mubarrokwildan1@gmail.com>
2025-11-16 21:50:52 +0700

    update final

--- app/Views/v_login_auth.php
+++ app/Views/v_login_auth.php
@@ -13,12 +13,9 @@
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/adminlte/plugins/fontawesome-free/css/all.min.css">
-    <!-- icheck bootstrap -->
-    <link rel="stylesheet" href="assets/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="assets/adminlte/dist/css/adminlte.min.css">
+   <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/fontawesome-free/css/all.min.css') ?>">
+   <link rel="stylesheet" href="<?= base_url('assets/adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
+   <link rel="stylesheet" href="<?= base_url('assets/adminlte/dist/css/adminlte.min.css') ?>">
 </head>
 
 <body class="hold-transition login-page">
@@ -36,7 +33,7 @@
                     </div>
                     <?php endif; ?>
 
-                <form action="<?php echo base_url('/auth') ?>" method="post">
+              <form action="<?= base_url('auth') ?>" method="post">
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="Username" name="username" value="<?= set_value('username') ?>">
                         <div class="input-group-append">
@@ -64,12 +61,9 @@
     </div>
     <!-- /.login-box -->
 
-    <!-- jQuery -->
-    <script src="assets/adminlte/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="assets/adminlte/dist/js/adminlte.min.js"></script>
-</body>
+<script src="<?= base_url('assets/adminlte/plugins/jquery/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js') ?>"></script>
+  
 
 </html>
